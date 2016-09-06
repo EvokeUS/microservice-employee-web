5361dae09616999d1e0b752416fa597a5befb6bb
vishal <apandiri@evoketechnologies.com>
2016-09-06 20:11:43 +0530

    Initial commits for microservice-employee-web

---
+++ microservice-employee-web/src/main/java/com/evoke/researchlabs/row/employee/beans/Employee.java
@@ -0,0 +1,31 @@
+package com.evoke.researchlabs.row.employee.beans;
+
+/**
+ * 
+ * @description EmployeeBean class act as a Pojo class
+ * @author P A VISHAL
+ * @version 1.0
+ * 
+ */
+public class Employee {
+	private String username;
+
+	private String emailId;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
